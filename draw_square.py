20384e34e8c70723484d77b9829092b113e1a483
John Stevenson <john@jr0cket.co.uk>
2014-04-14 20:52:28 +0100

    making hexagons with different colours and pensizes

--- draw_square.py
+++ draw_square.py
@@ -26,16 +26,27 @@ def draw_hexagon(line_length):
         turtle.forward(line_length)
         turtle.left(60)
 
-# Changing the pen color
-turtle.pencolor("blue")
+# Start the turtle higher up in the screen, so the hexagons pattern fits.
+def initialise_turtle():
+    turtle.penup()
+    turtle.setx(-50)
+    turtle.sety(100)
+    turtle.pendown()
+
+initialise_turtle()
+
 
 # Create 6 hexagons, moving to the bottom right corner to start a new one
 for i in range(6):
     turtle.pensize(i+3)
-    draw_hexagon(100)
-    turtle.forward(100)
-    turtle.right(60)
+    # Change the color of the pen before drawing each hexagon
+    for colorname in "Red", "Yellow", "Green", "Blue", "Orange", "Purple":
+        turtle.pencolor(colorname)
+        draw_hexagon(100)
+        turtle.forward(100)
+        turtle.right(60)
 
+# My loops are not quite right as the above draws 6 different coloured hexagons 6 times over at the different thickness levels
 
 # Stop the Python Turtle window from disappearing until we click on the window with the mouse
 turtle.exitonclick()
