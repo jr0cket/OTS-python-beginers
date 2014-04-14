aab18ea23478fb2590441de8b754173b6da7e031
John Stevenson <john@jr0cket.co.uk>
2014-04-14 13:29:18 +0100

    added more of an explination of the clickonexit() function of turtle as a comment

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ print("Hello Python World")
 # Display a simple hellow world as the title of the python turtle window
 turtle.title("Hello from the world of Python Turtles")
 
-# Stop the Python Turtle window from disappearing
+# Stop the Python Turtle window from disappearing until we click on the window with the mouse
 turtle.exitonclick()
 
 
